7eb30f051843efa263896edc6906166bc1123afc
Emily Yang <eyang42014@gmail.com>
2019-11-25 15:28:15 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,187 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Question from "./Component/Question";
+
+class App extends Component {
+
+  state = {
+    section:[
+      {
+        question: "I want to travel within the United State.",
+        ans_1:{text:"Yes", next_q: 0},
+        ans_1Class:"circle",
+        ans_2:{text:"No", next_q: 1, status:"circle"},
+        ans_2Class:"circle",
+        shown: true,
+        id:"section0",
+
+      },
+      {
+        question: "Where do I want to go? the beach or the forest?",
+        ans_1:{text:"Beach", next_q: 2, status:"circle"},
+        ans_1Class:"circle",
+        ans_2:{text:"Forest", next_q: 3, status:"circle"},
+        ans_2Class:"circle",
+        shown: false,
+        id:"section1",
+
+
+      },
+      {
+        question: "I want to stay in english speaking countries",
+        ans_1:{text:"Yes", next_q: 4},
+        ans_1Class:"circle",
+        ans_2:{text:"No", next_q: 5},
+        ans_2Class:"circle",
+        shown: false,
+        id:"section2",
+
+      },
+      {
+        question: "Do I want to be near a metropolitan area as well?",
+        ans_1:{text:"Yes", next_q: 6},
+        ans_1Class:"circle",
+        ans_2:{text:"No", next_q: 7},
+        ans_2Class:"circle",
+        shown: false,
+        id:"section3",
+
+      },
+      {
+        question: "Do I want to be near a metropolitan area as well?",
+        ans_1:{text:"Yes", next_q: 8},
+        ans_1Class:"circle",
+        ans_2:{text:"No", next_q: 9},
+        ans_2Class:"circle",
+        shown: false,
+        id:"section4",
+
+      },
+      {
+        question: "Do I have more than 5 days to travel?",
+        ans_1:{text:"Yes", next_q: 10},
+        ans_1Class:"circle",
+        ans_2:{text:"No", next_q: 11},
+        ans_2Class:"circle",
+        shown: false,
+        id:"section5",
+
+      },
+      {
+        question: "Am I more of a connoisser or a nature lover",
+        ans_1:{text:"Connoisser", next_q: 12},
+        ans_1Class:"circle",
+        ans_2:{text:"Nature Lover", next_q: 13},
+        ans_2Class:"circle",
+        shown: false,
+        id:"section6",
+
+      },
+    ]
+
+      
+  };
+
+  showNextOne = (idx) => {
+    let newSection = [...this.state.section];
+    newSection[idx+1].shown = true;
+    this.setState({
+      section:newSection
+    })
+  }
+
+  turnOff = (idx) => {
+    let newSection = [...this.state.section];
+    newSection[idx+1].shown = false;
+    newSection[idx].ans_1Class = "circle";
+    newSection[idx].ans_2Class = "circle";
+
+    newSection[idx+1].ans_1Class = "circle";
+    newSection[idx+1].ans_2Class = "circle";
+    this.setState({
+      section:newSection
+    })
+  }
+
+  circle1Pressed = (idx) => {
+    let newSection = [...this.state.section];
+    newSection[idx].ans_1Class = "circled";
+    newSection[idx].ans_2Class = "circle";
+    this.setState({
+      section:newSection
+    })
+  }
+
+  circle2Pressed = (idx) => {
+    let newSection = [...this.state.section];
+    newSection[idx].ans_1Class = "circle";
+    newSection[idx].ans_2Class = "circled";
+    this.setState({
+      section:newSection
+    })
+  }
+
+  backgroundPressed = (idx) => {
+    let newSection = [...this.state.section];
+    newSection[idx].ans_1Class = "circle";
+    newSection[idx].ans_2Class = "circle";
+    newSection[idx+1].shown = false; //possibly remove later if can get scrolling to work 
+    newSection[idx+2].shown = false; //possibly remove later if can get scrolling to work 
+    this.setState({
+      section:newSection
+    })
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        {/* <Question
+          section={this.state.section}
+        /> */}
+
+        
+        <ul>
+          {this.state.section.map((section, idx) => {
+            if (!section.shown) {
+              return (null);
+            }
+            return(
+              <Question
+                key={idx}
+                idx={idx}
+                id={section.id}
+                section={section}
+                checkClicked ={this.showNextOne}
+                turnOff ={this.turnOff}
+                circle1Pressed={this.circle1Pressed}
+                circle2Pressed={this.circle2Pressed}
+                backgroundPressed ={this.backgroundPressed}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+{/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
---
+++ src/Component/Question.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Question.css";
+import Circle from "./Circle"
+import { Link, animateScroll as scroll } from "react-scroll";
+
+
+
+const question = props => {
+    return (
+        <div className="section" onClick = {()=> {props.backgroundPressed(props.section.ans_1.next_q);}}>
+            <div className="question">
+                {props.section.question} 
+            </div>
+            <div className="ans-selection">
+            <Link
+                activeClass="active"
+                to={props.section.id}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration= {500}
+            >
+                <div className="ans">
+                    <Circle className={props.section.ans_1Class} onClick = {()=> { props.checkClicked(props.section.ans_1.next_q); props.turnOff(props.section.ans_2.next_q);props.circle1Pressed(props.idx)}}/>
+                    <div className="ans_text">
+                        {props.section.ans_1.text}
+                    </div>
+                </div>
+            </Link>
+            <Link
+                activeClass="active"
+                to="section1"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration= {500}
+            >
+                <div className="ans">
+                    <Circle className={props.section.ans_2Class} onClick = {()=> { props.checkClicked(props.section.ans_2.next_q); props.turnOff(props.section.ans_1.next_q); props.circle2Pressed(props.idx)}}/>
+                    <div className="ans_text">
+                        {props.section.ans_2.text}   {/*what would be the ideal way of editing the css of component */}
+                    </div>
+                </div>
+            </Link>
+            </div> 
+        </div>
+
+    );
+};
+
+//<div className="section" onClick = {(e)=> {e.stopPropagation();e.nativeEvent.stopImmediatePropagation();props.backgroundPressed(props.idx);}}>
+//somehow adding link fixed the problem??? 
+
+export default question;
