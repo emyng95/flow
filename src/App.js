7c77d8001ed53e01ad97976e4a4ccef55e3e185d
lxieyang <lxieyang@umich.edu>
2019-11-25 16:47:26 -0500

    fix navigation

--- src/App.js
+++ src/App.js
@@ -1,140 +1,198 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Element, animateScroll, scroller } from "react-scroll";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks
+} from "body-scroll-lock";
+
 //import logo from './logo.svg';
 
-import './App.css';
+import "./App.css";
 import Question from "./Component/Question";
 
-class App extends Component {
+const scrollDuration = 500;
 
+class App extends Component {
   state = {
-    section:[
+    previousQuestion: [],
+    currentQuestion: "section0",
+    nextQuestion: null,
+    isScrolling: false,
+    section: [
       {
         question: "I want to travel within the United State.",
-        ans_1:{text:"Yes", next_q: 0},
-        ans_1Class:"circle",
-        ans_2:{text:"No", next_q: 1, status:"circle"},
-        ans_2Class:"circle",
+        ans_1: { text: "Yes", next_q: "section1" },
+        ans_1Class: "circle",
+        ans_2: { text: "No", next_q: "section2", status: "circle" },
+        ans_2Class: "circle",
         shown: true,
-        id:"section0",
-
+        id: "section0"
       },
       {
         question: "Where do I want to go? the beach or the forest?",
-        ans_1:{text:"Beach", next_q: 2, status:"circle"},
-        ans_1Class:"circle",
-        ans_2:{text:"Forest", next_q: 3, status:"circle"},
-        ans_2Class:"circle",
+        ans_1: { text: "Beach", next_q: "section3", status: "circle" },
+        ans_1Class: "circle",
+        ans_2: { text: "Forest", next_q: "section4", status: "circle" },
+        ans_2Class: "circle",
         shown: false,
-        id:"section1",
-
-
+        id: "section1"
       },
       {
         question: "I want to stay in english speaking countries",
-        ans_1:{text:"Yes", next_q: 4},
-        ans_1Class:"circle",
-        ans_2:{text:"No", next_q: 5},
-        ans_2Class:"circle",
+        ans_1: { text: "Yes", next_q: "section5" },
+        ans_1Class: "circle",
+        ans_2: { text: "No", next_q: "section6" },
+        ans_2Class: "circle",
         shown: false,
-        id:"section2",
-
+        id: "section2"
       },
       {
         question: "Do I want to be near a metropolitan area as well?",
-        ans_1:{text:"Yes", next_q: 6},
-        ans_1Class:"circle",
-        ans_2:{text:"No", next_q: 7},
-        ans_2Class:"circle",
+        ans_1: { text: "Yes", next_q: 6 },
+        ans_1Class: "circle",
+        ans_2: { text: "No", next_q: 7 },
+        ans_2Class: "circle",
         shown: false,
-        id:"section3",
-
+        id: "section3"
       },
       {
         question: "Do I want to be near a metropolitan area as well?",
-        ans_1:{text:"Yes", next_q: 8},
-        ans_1Class:"circle",
-        ans_2:{text:"No", next_q: 9},
-        ans_2Class:"circle",
+        ans_1: { text: "Yes", next_q: 8 },
+        ans_1Class: "circle",
+        ans_2: { text: "No", next_q: 9 },
+        ans_2Class: "circle",
         shown: false,
-        id:"section4",
-
+        id: "section4"
       },
       {
         question: "Do I have more than 5 days to travel?",
-        ans_1:{text:"Yes", next_q: 10},
-        ans_1Class:"circle",
-        ans_2:{text:"No", next_q: 11},
-        ans_2Class:"circle",
+        ans_1: { text: "Yes", next_q: 10 },
+        ans_1Class: "circle",
+        ans_2: { text: "No", next_q: 11 },
+        ans_2Class: "circle",
         shown: false,
-        id:"section5",
-
+        id: "section5"
       },
       {
         question: "Am I more of a connoisser or a nature lover",
-        ans_1:{text:"Connoisser", next_q: 12},
-        ans_1Class:"circle",
-        ans_2:{text:"Nature Lover", next_q: 13},
-        ans_2Class:"circle",
+        ans_1: { text: "Connoisser", next_q: 12 },
+        ans_1Class: "circle",
+        ans_2: { text: "Nature Lover", next_q: 13 },
+        ans_2Class: "circle",
         shown: false,
-        id:"section6",
-
-      },
+        id: "section6"
+      }
     ]
-
-      
   };
 
-  showNextOne = (idx) => {
-    let newSection = [...this.state.section];
-    newSection[idx+1].shown = true;
-    this.setState({
-      section:newSection
-    })
+  targetRef = React.createRef();
+  targetElement = null;
+
+  componentDidMount() {
+    this.targetElement = this.targetRef.current;
+    disableBodyScroll(this.targetElement);
   }
 
-  turnOff = (idx) => {
-    let newSection = [...this.state.section];
-    newSection[idx+1].shown = false;
-    newSection[idx].ans_1Class = "circle";
-    newSection[idx].ans_2Class = "circle";
-
-    newSection[idx+1].ans_1Class = "circle";
-    newSection[idx+1].ans_2Class = "circle";
-    this.setState({
-      section:newSection
-    })
+  componentWillUnmount() {
+    clearAllBodyScrollLocks();
   }
 
-  circle1Pressed = (idx) => {
+  backButtonClickedHandler = (event, id) => {
+    event.stopPropagation();
+    enableBodyScroll(this.targetElement);
+    let previous = [...this.state.previousQuestion];
+    let next = previous[previous.length - 1];
+    console.log(next);
+    previous.pop();
+    this.setState({
+      previousQuestion: previous,
+      nextQuestion: next,
+      isScrolling: true
+    });
+
+    // start scrolling
+    scroller.scrollTo(next, {
+      duration: scrollDuration,
+      delay: 100,
+      smooth: true
+    });
+
+    // after scrolling
+    setTimeout(() => {
+      this.setState({
+        currentQuestion: next,
+        nextQuestion: null,
+        isScrolling: false
+      });
+      disableBodyScroll(this.targetElement);
+    }, scrollDuration + 100);
+  };
+
+  showNextOne = id => {
+    enableBodyScroll(this.targetElement);
+    console.log(id);
+    let current = this.state.currentQuestion;
+    let previous = [...this.state.previousQuestion];
+    previous.push(current);
+    this.setState({
+      previousQuestion: previous,
+      nextQuestion: id,
+      isScrolling: true
+    });
+
+    // start scrolling
+    scroller.scrollTo(id, {
+      duration: scrollDuration,
+      delay: 100,
+      smooth: true
+    });
+
+    // after scrolling
+    setTimeout(() => {
+      this.setState({
+        currentQuestion: id,
+        nextQuestion: null,
+        isScrolling: false
+      });
+      disableBodyScroll(this.targetElement);
+    }, scrollDuration + 100);
+  };
+
+  circle1Pressed = idx => {
     let newSection = [...this.state.section];
     newSection[idx].ans_1Class = "circled";
     newSection[idx].ans_2Class = "circle";
     this.setState({
-      section:newSection
-    })
-  }
+      section: newSection
+    });
+  };
 
-  circle2Pressed = (idx) => {
+  circle2Pressed = idx => {
     let newSection = [...this.state.section];
     newSection[idx].ans_1Class = "circle";
     newSection[idx].ans_2Class = "circled";
     this.setState({
-      section:newSection
-    })
-  }
+      section: newSection
+    });
+  };
 
-  backgroundPressed = (idx) => {
+  backgroundPressed = idx => {
     let newSection = [...this.state.section];
     newSection[idx].ans_1Class = "circle";
     newSection[idx].ans_2Class = "circle";
-    newSection[idx+1].shown = false; //possibly remove later if can get scrolling to work 
-    newSection[idx+2].shown = false; //possibly remove later if can get scrolling to work 
     this.setState({
-      section:newSection
-    })
-  }
+      section: newSection
+    });
+  };
 
-  render(){
+  render() {
+    const {
+      previousQuestion,
+      currentQuestion,
+      nextQuestion,
+      isScrolling
+    } = this.state;
 
     return (
       <div className="App">
@@ -142,25 +200,30 @@ class App extends Component {
           section={this.state.section}
         /> */}
 
-        
-        <ul>
+        <ul ref={this.targetRef}>
           {this.state.section.map((section, idx) => {
-            if (!section.shown) {
-              return (null);
+            if (
+              true
+              // section.id === currentQuestion ||
+              // (section.id == nextQuestion && isScrolling === true)
+            ) {
+              return (
+                <Element key={idx} name={section.id}>
+                  <Question
+                    idx={idx}
+                    id={section.id}
+                    section={section}
+                    checkClicked={this.showNextOne}
+                    turnOff={this.turnOff}
+                    circle1Pressed={this.circle1Pressed}
+                    circle2Pressed={this.circle2Pressed}
+                    backgroundPressed={this.backgroundPressed}
+                    backClicked={this.backButtonClickedHandler}
+                  />
+                </Element>
+              );
             }
-            return(
-              <Question
-                key={idx}
-                idx={idx}
-                id={section.id}
-                section={section}
-                checkClicked ={this.showNextOne}
-                turnOff ={this.turnOff}
-                circle1Pressed={this.circle1Pressed}
-                circle2Pressed={this.circle2Pressed}
-                backgroundPressed ={this.backgroundPressed}
-              />
-            );
+            return null;
           })}
         </ul>
       </div>
@@ -170,8 +233,8 @@ class App extends Component {
 
 export default App;
 
-
-{/* <header className="App-header">
+{
+  /* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -184,4 +247,5 @@ export default App;
           >
             Learn React
           </a>
-        </header> */}
+        </header> */
+}
--- src/Component/Circle.js
+++ src/Component/Circle.js
@@ -1,25 +1,23 @@
-
-import posed from 'react-pose';
+import posed from "react-pose";
 
 const Circle = posed.div({
   hoverable: true,
   pressable: true,
   init: {
     scale: 1,
-    boxShadow: "0 0 60px 10px rgba(255, 255, 255, 0.473),0 0 100px 5px rgba(255, 95, 95, 0.438), 0 0 140px 2px rgba(255, 255, 255, 0.24)"
+    boxShadow:
+      "0 0 60px 10px rgba(255, 255, 255, 0.473),0 0 100px 5px rgba(255, 95, 95, 0.438), 0 0 140px 2px rgba(255, 255, 255, 0.24)"
   },
   hover: {
     scale: 1.6,
-    boxShadow: "0 0 60px 10px rgba(255, 95, 95, 0.438),0 0 100px 5px rgba(255, 95, 95, 0.438), 0 0 140px 2px rgba(255, 255, 255, 0.24)"
+    boxShadow:
+      "0 0 60px 10px rgba(255, 95, 95, 0.438),0 0 100px 5px rgba(255, 95, 95, 0.438), 0 0 140px 2px rgba(255, 255, 255, 0.24)"
   },
   press: {
     scale: 1.2,
-    boxShadow: "0 0 60px 10px rgba(255, 95, 95, 0.438),0 0 100px 5px rgba(255, 95, 95, 0.438), 0 0 140px 2px rgba(255, 255, 255, 0.24)"
+    boxShadow:
+      "0 0 60px 10px rgba(255, 95, 95, 0.438),0 0 100px 5px rgba(255, 95, 95, 0.438), 0 0 140px 2px rgba(255, 255, 255, 0.24)"
   }
-
-
-
 });
-
 
 export default Circle;
